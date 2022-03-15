af885b4ac015d622f85bafef469848302a12e4b4
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2022-03-15 20:22:15 +0700

    Modification to Records

--- src/blockchain/Records.java
+++ src/blockchain/Records.java
@@ -1,5 +1,7 @@
 package blockchain;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,32 @@ public class Records {
 
     public void add(String records) {
         dataLst.add(records);
+    }
+
+    @Override
+    public String toString() {
+        return "Records [" + dataLst + "]";
+    }
+
+    public List<String> readRecordsFile(String LEDGER_FILE) {
+        List<String> datas = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(LEDGER_FILE))) {
+            for (String line; (line = br.readLine()) != null;) {
+                datas.add(line);
+            }
+        } catch (Exception e) {
+
+        }
+
+        return datas;
+    }
+
+    public int size() {
+        return dataLst.size();
+    }
+
+    public void clear(){
+        dataLst.clear();
     }
 
 }
